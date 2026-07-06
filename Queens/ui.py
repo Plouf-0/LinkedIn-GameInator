3c609f79ae46a5bee6e5bb507e0d0704ab806e4b
Valentin Giaufer <valentin.giaufer@gmail.com>
2026-07-06 16:54:53 +0200

    feat: implement archiving functionality for queens grid with file creation and logging

--- Queens/ui.py
+++ Queens/ui.py
@@ -1,9 +1,13 @@
 from typing import List
-from .queens_grid import Cell
+from .queens_grid import Cell, convert_color
+from pathlib import Path
+from datetime import date as dt
 
 EMPTY = 0
 QUEEN = 1
 BLOCKED = -1
+
+ARCHIVE_PATH = "Queens\\Archive"
 
 
 def print_grid(grid: List[List[Cell]]) -> None:
@@ -49,10 +53,12 @@ def print_grid(grid: List[List[Cell]]) -> None:
             print("\033[0m", end="")
         print("\033[0m ")
 
+
 def print_regions(regions: List[List[Cell]]) -> None:
     print("Found regions (list of coords per color):")
     for i, region in enumerate(regions):
         print(f"Region {i}: {region}")
+
 
 def print_color_palette() -> None:
     for style in [0, 1]:  # 0: normal, 1: bold/bright
@@ -62,4 +68,43 @@ def print_color_palette() -> None:
                 print(f"\033[{code}m {code} \033[0m", end=" ")
             print()  # Newline after each row
         print()  # Extra newline between normal and bold
+    return
+
+
+def find_or_create_archive(filename: str) -> bool:
+    """Find the archive file with the given name in the current directory.
+
+    Output: True if the file already exists, False if it was created.
+    """
+    path = Path(f"{ARCHIVE_PATH}/{filename}_Queens.txt")
+
+    if not path.exists():
+        with open(path, "w", encoding="utf-8") as f:
+            f.write(f"Archive of the LinkedIn's game Queens on the day of {filename}\n")
+        return False
+    else:
+        print("File already exists.")
+        return True
+
+
+def achive_queens_grid(grid: List[List[Cell]], opt_filename: str = "") -> None:
+    """Archive the current state of the grid to a text file."""
+    today: str = str(dt.today())
+
+    if opt_filename == "":
+        path = Path(f"{ARCHIVE_PATH}/{today}_Queens.txt")
+        if find_or_create_archive(today):
+            return
+    else:
+        path = Path(f"{ARCHIVE_PATH}/{opt_filename}_Queens.txt")
+        if find_or_create_archive(opt_filename):
+            return
+
+    with open(path, "a", encoding="utf-8") as f:
+        f.write(f"Today's grid size is {len(grid)}x{len(grid[0])}.\n\n")
+        for r in grid:
+            row: str = ""
+            for cell in r:
+                row += convert_color(cell.color) + " "
+            f.write(row.strip() + "\n")
     return
